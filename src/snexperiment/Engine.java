314748cff1354937d2361b377f21589196c5921d
Dssmirnov-msc <61143611+Dssmirnov-msc@users.noreply.github.com>
2022-04-27 17:23:56 +0300

    Basic Classes
    Создал базовые классы, без реализации поведения

---
+++ src/snexperiment/Engine.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snexperiment;
+
+/**
+ *
+ * @author Dmitry
+ */
+public class Engine {
+    private Scenario scenario;
+
+    public Engine(Scenario scenario) {
+        this.scenario = scenario;
+    }
+    public Scenario getScenario() {
+        return scenario;
+    }
+    public void setScenario(Scenario scenario) {
+        this.scenario = scenario;
+    }
+    
+
+    
+    
+}
---
+++ src/snexperiment/Scenario.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snexperiment;
+
+/**
+ *
+ * @author Dmitry
+ */
+public class Scenario {
+    
+    private Ratio ratiohandler;
+    
+    public Scenario() {
+        
+    }
+    
+    
+    
+    public void populate(){}
+            
+    public void registrate(){}
+
+    private static class Ratio {
+
+        public Ratio() {
+        }
+    }
+    
+}
